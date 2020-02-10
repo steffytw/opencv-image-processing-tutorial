e4d99b87ddffb478192f0c26ba7443a54f4f22a2
steffytw <steffythankamwilson@cs.ajce.in>
2020-02-10 14:43:25 +0530

    mouseevent updated

--- drawline_dl.py
+++ drawline_dl.py
@@ -4,15 +4,17 @@ import cv2
 # Create a black image
 img = np.zeros((512, 512, 3), np.uint8)
 
+
+
 # Draw a diagonal blue line with thickness of 5 px
-# img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
-#
-# cv2.imshow("image", img);
-# cv2.waitKey(5000)
-# cv2.destroyAllWindows()
+img = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
+
+cv2.imshow("image", img);
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
 
 # Create a rectangle image
-# img = cv2.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)
+img = cv2.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)
 
 # cv2.imshow("image", img);
 # cv2.waitKey(5000)
@@ -20,13 +22,19 @@ img = np.zeros((512, 512, 3), np.uint8)
 # cv2.imshow("image", img).waitKey(0);
 # Create a circle image
 img = cv2.circle(img,(447,63), 63, (0,0,255), -1)
-cv2.imshow("image", img);
-cv2.waitKey(5000)
-cv2.destroyAllWindows()
+# cv2.imshow("image", img);
+# cv2.waitKey(5000)
+# cv2.destroyAllWindows()
 #
-# img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
+# cv2.imshow("image", img);
+# cv2.waitKey(5000)
+# cv2.destroyAllWindows()
 
 
-# pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
-# pts = pts.reshape((-1,1,2))
-# img = cv2.polylines(img,[pts],True,(0,255,255))
+pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+img = cv2.polylines(img,[pts],True,(0,255,255))
+cv2.imshow("image", img);
+cv2.waitKey(10000)
+cv2.destroyAllWindows()

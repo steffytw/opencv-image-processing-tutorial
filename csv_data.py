43b205c81cf77e107b7ebe4424799e368d7adc6b
steffytw <steffythankamwilson@cs.ajce.in>
2020-02-13 10:42:29 +0530

    image processing data updation

---
+++ csv_data.py
@@ -0,0 +1,7 @@
+# Statistical Summary
+from pandas import read_csv
+url = 'https://goo.gl/bDdBiA'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(url, names=names)
+description = data.describe()
+print(description)
